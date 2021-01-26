00f35dc40218cb5c982f59e58133d85c56610890
Rahul rc <60568652+HACKE-RC@users.noreply.github.com>
2021-01-26 11:27:22 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from os import system
+
+try:
+    import jinja2
+    import yaml
+except:
+    system("pip3 install -r requirements.txt")
+    system("cp main.py /usr/bin/ReporterX && chmod 755 /usr/bin/ReporterX")
+
+print("[INF] The tool has been successfully installed! [INF}")
+print("Now you can access this tool from anywhere by typing RepoterX.")
+print("Start now by typing ReporterX -h")
