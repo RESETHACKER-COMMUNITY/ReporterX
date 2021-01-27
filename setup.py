9f6010df52c90dbab103c66fd47cd105f2bc49ff
Rahul rc <60568652+HACKE-RC@users.noreply.github.com>
2021-01-27 10:13:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
     system("pip3 install -r requirements.txt")
     system("cp main.py /usr/bin/ReporterX && chmod 755 /usr/bin/ReporterX")
 
-environ['reporterxpath'] = getcwd()
-print("[INF] The tool has been successfully installed! [INF}")
+ 
+print("[INF] The tool has been successfully installed! [INF]")
 print("Now you can access this tool from anywhere by typing RepoterX.")
 print("Start now by typing ReporterX -h")
