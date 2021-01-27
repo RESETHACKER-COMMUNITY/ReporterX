455718550f512023bce0aa2e8fe9c8cc01fb8a21
Rahul rc <60568652+HACKE-RC@users.noreply.github.com>
2021-01-27 10:13:50 +0530

    Update setup-termux.py

--- setup-termux.py
+++ setup-termux.py
@@ -7,7 +7,6 @@ except:
     system("pip3 install -r requirements.txt")
     system("cp main.py /data/data/com.termux/files/usr/bin/ReporterX && chmod 755 /data/data/com.termux/files/usr/bin/ReporterX")
 
-environ['reporterxpath'] = getcwd()
 print("[INF] The tool has been successfully installed! [INF}")
 print("Now you can access this tool from anywhere by typing RepoterX.")
 print("Start now by typing ReporterX -h")
